e3092c6f31ebe18d81ec72bd6d6d84a1403ef5f1
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-03-02 15:14:36 +0200

    Add stuff.

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setTemplateFormats([
+    "md",
+    "html",
+    "css", // css is not yet a recognized template extension in Eleventy
+  ]);
+};
