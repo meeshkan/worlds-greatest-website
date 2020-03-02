06c6efac41bb995de709c6cfbb61a066bef52ea5
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-03-02 16:05:40 +0200

    Remove comment.

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats([
     "md",
     "html",
-    "css", // css is not yet a recognized template extension in Eleventy
+    "css",
     "njk"
   ]);
 
