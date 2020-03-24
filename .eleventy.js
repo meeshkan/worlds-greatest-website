6995ff0ff7201da8b9e48eecb31c152a4ff06d30
Makenna Smutz <makennasmutz@gmail.com>
2020-03-24 14:42:46 +0100

    Add in syntax highlighting with the 11ty plugin (#14)

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,13 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setTemplateFormats([
-    "md",
-    "html",
-    "css",
-    "njk"
-  ]);
+const syntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setTemplateFormats(["md", "html", "css", "njk"]);
 
   eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addFilter("readableDate", dateObj => dateObj.toLocaleDateString());
+  eleventyConfig.addFilter("readableDate", dateObj =>
+    dateObj.toLocaleDateString()
+  );
+
+  eleventyConfig.addPlugin(syntaxHighlightPlugin);
 };
